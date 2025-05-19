503c68fc6e268185137bf16a892ac75becd411f8
xiezhiyan <16297996+xiezhiyan@users.noreply.github.com>
2025-05-19 15:55:12 +0800

    sdk工程命名修改

--- android/app/src/main/java/com/awesomeproject0752/MainActivity.java
+++ android/app/src/main/java/com/awesomeproject0752/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "AwesomeProject0752";
+    return "MapPlusWeb3DSDK";
   }
 
   /**
